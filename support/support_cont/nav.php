7ea19debaf4db7e56c33095d5e3fe692ae4fc550
loveopo1999@gmail.com <loveopo1999@gmail.com>
2024-02-08 09:28:22 +0700

    updata

---
+++ support/support_cont/nav.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-example navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="javascript:void(0)">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-ex-2" aria-controls="navbar-ex-2" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar-ex-2">
+            <div class="navbar-nav me-auto">
+                <a class="nav-item nav-link active" href="javascript:void(0)">Home</a>
+                <a class="nav-item nav-link" href="javascript:void(0)">About</a>
+                <a class="nav-item nav-link" href="javascript:void(0)">Contact</a>
+            </div>
+
+            <span class="navbar-text">Marshmallow brownie lemon drops cheesecake.</span>
+        </div>
+    </div>
+</nav>
